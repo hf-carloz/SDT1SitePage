37f9066de6d688eeb9e908a004fe41b75df7441d
hf-carloz <hf.carloz@gmail.com>
2017-08-15 13:41:16 +0800

    hghghghg

---
+++ WebApplication1/WebApplication1/Index/customscripts/JavaScript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".project").addClass("hide");
+    $("#project_content").removeClass("hide");
+    $("#projects").removeClass("hide");
+
+
+    $(".project_icons td > img").click(function () {
+        $("#projects").addClass("hide");
+        $("#JComFITS").removeClass("hide");
+    });
+
+    $("a[title='back']").click(function () {
+        $(this).parent().addClass("hide");
+
+    });
+});
